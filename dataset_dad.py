d51e92e1373e6ee4b9cfd972ad2a4175f73002ff
Vipooshan Vipulananthan <77396714+Vipooshan-1998@users.noreply.github.com>
2025-09-28 14:49:42 +0530

    Update dataset_dad.py

--- dataset_dad.py
+++ dataset_dad.py
@@ -164,6 +164,8 @@ class Dataset(Dataset):
         #                                     feature_path.split('/')[-1].split(".")[0][:] + '.npy')
         frame_stats = torch.from_numpy(np.load(frame_stats_file)).float()
 
+        print(feature_path.split('/')[-1].split(".")[0].split("_")[5:11])
+        print("---------------------")
         # Attention
         if curr_vid_label > 0:
             att_file = os.path.join(self.attention_path, feature_path.split('/')[-2], "positive",
@@ -521,6 +523,7 @@ class FeaturesDataset(Dataset):
 
     def __len__(self):
         return len(self.feature_paths)
+
 
 
 
