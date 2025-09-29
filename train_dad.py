8ea87c654125c73e465fee18d89733a41f8b102f
Vipooshan Vipulananthan <77396714+Vipooshan-1998@users.noreply.github.com>
2025-09-29 09:57:55 +0530

    Update train_dad.py

--- train_dad.py
+++ train_dad.py
@@ -94,8 +94,8 @@ def test_model(epoch, model, test_dataloader):
 			# logits, probs = model(X, edge_index, img_feat, video_adj_list, edge_embeddings, temporal_adj_list, temporal_edge_w, batch_vec)
 			logits, probs, Ht = model(img_feat, obj_feat, obj_boxes, driver_attn_map=all_att_feat, driver_attn_per_obj=None)
 
-		print(logits.shape)
-		print(probs.shape)
+		print("logits.shape: ", logits.shape)
+		print("probs.shape:", probs.shape)
 		
 		# logits: (B, T, 2)
 		pred_labels = probs.argmax(-1)       # (B, T)
@@ -340,6 +340,7 @@ def main():
 	
 if __name__ == "__main__":
 	main()
+
 
 
 
