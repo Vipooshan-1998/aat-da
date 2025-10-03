8c7331954a036688a6e9e6f77341ddc814e3f375
Vipooshan Vipulananthan <77396714+Vipooshan-1998@users.noreply.github.com>
2025-10-03 10:53:58 +0530

    Update train_dota.py

--- train_dota.py
+++ train_dota.py
@@ -219,7 +219,7 @@ def main():
     test_dataloader = DataLoader(test_dataset, batch_size=opt.test_video_batch_size, shuffle=False, num_workers=8)
 
     # Define network
-    model = SpaceTempGoG_detr_dota(input_dim=opt.input_dim, embedding_dim=opt.embedding_dim,
+    model = AAT_DA_FullSeq(input_dim=opt.input_dim, embedding_dim=opt.embedding_dim,
                                   img_feat_dim=opt.img_feat_dim, num_classes=opt.num_classes).to(device)
     print(model)
 
