eaa5e9225e8420b0470154e5511f93fb125d0370
Vipooshan Vipulananthan <77396714+Vipooshan-1998@users.noreply.github.com>
2025-11-07 21:17:30 +0530

    Update dataset_nexar.py

--- dataset_nexar.py
+++ dataset_nexar.py
@@ -198,11 +198,11 @@ class Dataset(Dataset):
 
         # Attention
         if curr_vid_label > 0:
-            att_file = os.path.join(self.attention_path, feature_path.split('/')[-2], "positive",
+            att_file = os.path.join(self.attention_path, feature_path.split('/')[-2][:-3], "positive",
                                     feature_path.split('/')[-1].split(".")[0][5:] + '.mp4')
         else:
-            att_file = os.path.join(self.attention_path, feature_path.split('/')[-2], "negative",
-                                    feature_path.split('/')[-1].split(".")[0][5:] + '-neg.mp4')
+            att_file = os.path.join(self.attention_path, feature_path.split('/')[-2][:-3], "negative",
+                                    feature_path.split('/')[-1].split(".")[0][5:] + '.mp4')
         # all_att_feat = self.transform(np.load(att_file)).squeeze(0)
 
         # # # Read video frames (T x H x W x C)
@@ -333,6 +333,7 @@ class Dataset(Dataset):
     def __len__(self):
         return len(self.feature_paths)
     
+
 
 
 
