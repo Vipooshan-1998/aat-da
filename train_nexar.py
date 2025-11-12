a98424ec14dc1f61a2a8d99bbe8781c07c8f80bd
Vipooshan Vipulananthan <77396714+Vipooshan-1998@users.noreply.github.com>
2025-11-12 20:04:11 +0530

    Update train_nexar.py

--- train_nexar.py
+++ train_nexar.py
@@ -22,7 +22,7 @@ import matplotlib.pyplot as plt
 import time
 from eval_utils import evaluation
 
-from thop import profile
+# from thop import profile
 
 torch.manual_seed(0)   #3407
 
@@ -242,13 +242,13 @@ def main():
 			# print(all_att_feat.shape)
 			all_att_feat = all_att_feat.float()
 			# logits, probs = model(X, edge_index, img_feat, video_adj_list, edge_embeddings, temporal_adj_list, temporal_edge_w, batch_vec)
-			# logits, probs, Ht = model(img_feat, obj_feat, obj_boxes, driver_attn_map=all_att_feat, driver_attn_per_obj=None)
+			logits, probs, Ht = model(img_feat, obj_feat, obj_boxes, driver_attn_map=all_att_feat, driver_attn_per_obj=None)
 
-			# FLOPs calculation
-			inputs = (img_feat, obj_feat, obj_boxes, all_att_feat, None)      # match forward signature
-			flops, params = profile(model, inputs=inputs)
-			print(f"Total FLOPs: {flops}")           
-			print(f"Total Params: {params}") 
+			# # FLOPs calculation
+			# inputs = (img_feat, obj_feat, obj_boxes, all_att_feat, None)      # match forward signature
+			# flops, params = profile(model, inputs=inputs)
+			# print(f"Total FLOPs: {flops}")           
+			# print(f"Total Params: {params}") 
 
 			logits = logits.squeeze(0)
 			probs = probs.squeeze(0)
@@ -309,6 +309,7 @@ def main():
 	
 if __name__ == "__main__":
 	main()
+
 
 
 
